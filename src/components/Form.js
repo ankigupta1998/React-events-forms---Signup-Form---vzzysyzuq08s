c78030b8f8c71a871b342abe2a1ebca790dfffea
ankigupta1998 <95403786+ankigupta1998@users.noreply.github.com>
2022-04-05 23:08:20 +0530

    Greeting Added

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,10 +2,19 @@ import React from "react";
 // import { useState } from "react";
 import "./Form.css";
 import useCustom from "./customHook/useCustom";
+import { useState } from "react";
 
 let RegEx = /^[a-z0-9 ]+$/i;
 const Form = () => {
-  
+  const[test,setTest]=useState(false);
+
+  const clickHandler=()=>{
+    !valueIsValidForPass &&
+      !valueIsValidForNo &&
+      !valueIsValidForOpt &&
+      !valueIsValidForEmail &&
+      !valueIsValid&&setTest(true);
+  }
   const submitHandler = (e) => {
     e.preventDefault();
     setIsTouched(true);
@@ -23,7 +32,7 @@ const Form = () => {
     inputblurHandler,
     setIsTouched,
   } = useCustom((value) => {
-    return  !RegEx.test(value) || value.trim()==="" ;
+    return !RegEx.test(value) || value.trim() === "";
   });
 
   const {
@@ -60,14 +69,13 @@ const Form = () => {
   });
 
   const {
-
-    value:valueOpt,
-    valueIsValid:valueIsValidForOpt,
-    isTouched:isTouchedForOpt,
-    valueChangeHandler:valueChangeHandlerForopt,
+    value: valueOpt,
+    valueIsValid: valueIsValidForOpt,
+    isTouched: isTouchedForOpt,
+    valueChangeHandler: valueChangeHandlerForopt,
     inputblurHandler: inputblurHandlerForOpt,
     setIsTouched: setIsTouchedForOpt,
-  } = useCustom((valueOpt)=>{
+  } = useCustom((valueOpt) => {
     return !valueOpt;
   });
 
@@ -86,89 +94,101 @@ const Form = () => {
       ? "name-input-invalid"
       : "name-input";
   const OptValidClass =
-  isTouchedForOpt&&valueIsValidForOpt?"name-input-invalid"
-  : "name-input";
-
+    isTouchedForOpt && valueIsValidForOpt ? "name-input-invalid" : "name-input";
 
   const email = valueEmail;
-  const result = email.slice(0, email.indexOf('@'));
+  const result = email.slice(0, email.indexOf("@"));
+ 
+  
 
   return (
     <>
       <div className="container">
-        <form onSubmit={submitHandler} >
+        <form onSubmit={submitHandler}>
           <div className="content">
-            
-          <label htmlFor="name">Name:  </label>
-          <input
-            className={inputValidClass}
-            type="text"
-            data-testid="name"
-            id="name"
-            onChange={valueChangeHandler}
-            value={value}
-            onBlur={inputblurHandler}
+            <label htmlFor="name">Name: </label>
+            <input
+              className={inputValidClass}
+              type="text"
+              data-testid="name"
+              id="name"
+              onChange={valueChangeHandler}
+              value={value}
+              onBlur={inputblurHandler}
             />
-          <br />
-        
-          {isTouched && valueIsValid && <div>Name is not alphanumeric</div>}
-          <label htmlFor="email">Email: </label>
-          <input
-            className={emailValidClass}
-            type="text"
-            id="email"
-            data-testid="email"
-            value={valueEmail}
-            onChange={valueChangeHandlerForEmail}
-            onBlur={inputblurHandlerForEmail}
+            <br />
+
+            {isTouched && valueIsValid && <div>Name is not alphanumeric</div>}
+            <label htmlFor="email">Email: </label>
+            <input
+              className={emailValidClass}
+              type="text"
+              id="email"
+              data-testid="email"
+              value={valueEmail}
+              onChange={valueChangeHandlerForEmail}
+              onBlur={inputblurHandlerForEmail}
             />
-          <br />
-          {isTouchedForEmail && valueIsValidForEmail && (
-            <div>Email must contain @</div>
-          )}
-          <label htmlFor="gender">Gender:</label>
-          <select className={OptValidClass} data-testid="gender" id="gender" defaultValue="Male" onBlur={inputblurHandlerForOpt} onClick={valueChangeHandlerForopt} >
-            <option>Male</option>
-            <option>Female</option>
-            <option>others</option>
-          </select>
-          <br />
-          {isTouchedForOpt && valueIsValidForOpt &&<div>Please identify as male, female or others</div>}
-          <label htmlFor="number">Number:</label>
-          <input
-            className={NoValidClass}
-            type="number"
-            id="number"
-            data-testid="phoneNumber"
-            onChange={valueChangeHandlerForNo}
-            value={valueNo}
-            onBlur={inputblurHandlerForNo}
-          />
-          <br />
-          {isTouchedForNo && valueIsValidForNo && (
-            <div>Phone Number must contain only numbers</div>
-          )}
-          <label htmlFor="pass">Password:</label>
-          <input
-            className={PassValidClass}
-            type="password"
-            onChange={valueChangeHandlerForPass}
-            onBlur={inputblurHandlerForPass}
-            id="pass"
-            data-testid="password"
-            />
-          <br />
-          {isTouchedForPass && valueIsValidForPass && (
-            <div>Password must contain atleast 6 letters</div>
+            <br />
+            {isTouchedForEmail && valueIsValidForEmail && (
+              <div>Email must contain @</div>
             )}
-          <button type="submit" data-testid="submit" onClick={()=>{
-            !valueIsValidForPass && !valueIsValidForNo && !valueIsValidForOpt && !valueIsValidForEmail && !valueIsValid && alert ( `Hello ${result} `)
-          }} >
-            Submit
-          </button>
-            </div>
+            <label htmlFor="gender">Gender:</label>
+            <select
+              className={OptValidClass}
+              data-testid="gender"
+              id="gender"
+              defaultValue="Male"
+              onBlur={inputblurHandlerForOpt}
+              onClick={valueChangeHandlerForopt}
+            >
+              <option>Male</option>
+              <option>Female</option>
+              <option>others</option>
+            </select>
+            <br />
+            {isTouchedForOpt && valueIsValidForOpt && (
+              <div>Please identify as male, female or others</div>
+            )}
+            <label htmlFor="number">Number:</label>
+            <input
+              className={NoValidClass}
+              type="number"
+              id="number"
+              data-testid="phoneNumber"
+              onChange={valueChangeHandlerForNo}
+              value={valueNo}
+              onBlur={inputblurHandlerForNo}
+            />
+            <br />
+            {isTouchedForNo && valueIsValidForNo && (
+              <div>Phone Number must contain only numbers</div>
+            )}
+            <label htmlFor="pass">Password:</label>
+            <input
+              className={PassValidClass}
+              type="password"
+              onChange={valueChangeHandlerForPass}
+              onBlur={inputblurHandlerForPass}
+              id="pass"
+              data-testid="password"
+            />
+            <br />
+            {isTouchedForPass && valueIsValidForPass && (
+              <div>Password must contain atleast 6 letters</div>
+            )}
+            <button type="submit" data-testid="submit" onClick={clickHandler}>
+              Submit
+            </button>
+          </div>
         </form>
+        
       </div>
+      {test && !valueIsValidForPass &&
+      !valueIsValidForNo &&
+      !valueIsValidForOpt &&
+      !valueIsValidForEmail &&
+      !valueIsValid && <h1 className="greetingMessage">Hello {result}</h1>}
     </>
   );
 };
